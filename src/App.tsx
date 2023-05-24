e8da1f42c950d1974774fee2b2fc912a9014a81c
Emmanuel Ezekiel <emamex44@gmail.com>
2023-05-24 04:26:48 +0100

    setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./styles/App.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // RouteComponentProps
+} from "react-router-dom";
+import Login from "./pages/Login/login";
+import Dashboard from "./pages/Dashboard/dashboard";
+import User from "./pages/user/user";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="users" element={<User />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
